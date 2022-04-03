c64ffa2499c754cef42a88153f41749838eaa373
Pablo P. Galvão <98771845+pablopgalvao@users.noreply.github.com>
2022-04-03 10:04:30 -0300

    Inserindo carregamento automatico

--- index.py
+++ index.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/usuario/<nome_usuario>')
+def index():
+    return render_template("usuario.html", nome_usuario=nomeusuario)
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
